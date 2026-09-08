1210eacc0d3e9e758182302cfa7f42d2cc57bb85
BipolarSheep <123794873+BipolarSheep@users.noreply.github.com>
2026-09-08 16:38:18 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'reloj-v1';
+const CACHE = 'reloj-v2';
 const ARCHIVOS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
